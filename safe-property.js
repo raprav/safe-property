a9bf110be2c6925bba8a08216009b25b675b989b
Rafa Prats <estarp@gmail.com>
2018-06-14 22:46:51 +0200

    fix: export

--- safe-property.js
+++ safe-property.js
@@ -1,4 +1,9 @@
-module.export = (arr, path, defaultValue) => {
+module.exports = (arr, path, defaultValue) => {
+  if (typeof path === 'string' || path instanceof String) {
+    path = path.split('.').map((p) => {
+      return p.trim();
+    })
+  }
   let obj = arr[path[0]];
   for (let i = 1; i < path.length && obj; i++) {
     obj = obj[path[i]];
